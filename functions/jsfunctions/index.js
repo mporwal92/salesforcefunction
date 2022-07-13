dbbf444f35de2306f88be42048cb826408fa990f
Manish Porwal <mporwal92@gmail.com>
2022-07-13 21:44:10 +0530

    functionchange

--- functions/jsfunctions/index.js
+++ functions/jsfunctions/index.js
@@ -10,11 +10,50 @@
  * @param logger: logging handler used to capture application logs and trace specifically
  *                 to a given execution of a function.
  */
- 
+
+import fastify from "fastify-js";
+import jsonruleengine from "json-rules-engine-js";
 export default async function (event, context, logger) {
+
+    const fastify = fastify();
+    const engine = jsonruleengine();
+
+    engine.addRule({
+        conditions: {
+          all: [
+            {
+              fact: "temperature",
+              operator: "equal",
+              value: 100
+            }
+          ]
+        },
+        onSuccess(){
+          console.log('on success called')
+        },
+        onFailure(){
+          console.log('on failure called')
+        },
+        event: {
+          type: "message",
+          params: {
+            data: "hello-world!"
+          }
+        }
+      });
+      
+    const facts = { temperature: 100 };
+    engine
+    .run(facts)
+    .then(results => {
+        results.events.map(event => console.log('value', event.params.data));
+    })
+    .catch((error)=> console.log('err is', error));
+    
     logger.info(`Invoking Jsfunctions with payload ${JSON.stringify(event.data || {})}`);
 
     const results = await context.org.dataApi.query('SELECT Id, Name FROM Account');
+
 
     logger.info(JSON.stringify(results));
 
